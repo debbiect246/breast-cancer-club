12db1d15a4ac354ed0c1926c0265ba2d759386c9
tindyc <tindyc@bath.edu>
2021-11-12 21:13:05 +0000

    Add authentication system and connect to mongodb

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, render_template, request, redirect, flash, url_for
-
-
+from datetime import date, datetime
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 # envWS.py should exist only in Development
 if os.path.exists("envWS.py"):
     import envWS
@@ -15,12 +19,92 @@ app.config["FLASK_IP"] = os.environ.get("FLASK_IP",   "0.0.0.0")
 app.config["FLASK_PORT"] = int(os.environ.get("PORT"))
 app.config["FLASK_DEBUG"] = os.environ.get("FLASK_DEBUG", "False").lower() \
                             in {'1', 'true', 't', 'yes', 'y'}
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+date = date.today()
 
 # App routes
 # ==============
 @app.route("/")  # trigger point through webserver: "/"= root directory home page
 def index():
     return render_template("index.html")
+
+# Register user in the db
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # Check if user already in db
+        existing_user = mongo.db.users.find_one(
+                        {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists!")
+            return redirect(url_for("register"))
+
+        # check if email already in db
+        existing_email = mongo.db.users.find_one(
+                        {"email": request.form.get("email").lower()})
+
+        if existing_email:
+            flash("Email address already registered!")
+            return redirect(url_for("register"))
+
+        # add user details to db
+        register = {
+            "firstname": request.form.get("firstname").lower(),
+            "lastname": request.form.get("lastname").lower(),
+            "username": request.form.get("username").lower(),
+            "email": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "date_created": date.strftime("%d %b %Y")
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the user into a session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign Up Successful!")
+        return redirect(url_for("my_profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+# Log existing user into site
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if user already in db
+        existing_user = mongo.db.users.find_one(
+                        {"username": request.form.get("username").lower()})
+        # ensure password matches
+        if existing_user:
+            if check_password_hash(existing_user["password"],
+                                   request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                return redirect(url_for("my_profile",
+                                username=session["user"]))
+            else:
+                # invalid password
+                flash("Incorrect Username/Password!")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist/is incorrect
+            flash("Incorrect Usernname/Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+# Logs user out of their account
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 #blog view
 @app.route("/blog")
