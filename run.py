ea29cb45a6a487472060c424235045f654948279
tindyc <tindyc@bath.edu>
2021-11-14 16:28:45 +0000

    add profile and blog functionality and contact page

--- run.py
+++ run.py
@@ -334,8 +334,8 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=False)
+    port=int(os.environ.get("PORT")),
+    debug=True)
     
 
 
