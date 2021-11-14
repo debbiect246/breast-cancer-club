1958d83798686f158e5ade129c1d09ee6cd3134b
tindyc <tindyc@bath.edu>
2021-11-14 14:18:46 +0000

    Add blog section

--- run.py
+++ run.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 
 # take app configuration from OS environment variables
-app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 
+app.secret_key = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
@@ -52,83 +52,73 @@ def find_id():
 def index():
     return render_template("index.html")
 
-# Register user in the db
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        # Check if user already in db
-        existing_user = mongo.db.users.find_one(
-                        {"username": request.form.get("username").lower()})
-
-        if existing_user:
-            flash("Username already exists!")
-            return redirect(url_for("register"))
-
-        # check if email already in db
-        existing_email = mongo.db.users.find_one(
-                        {"email": request.form.get("email").lower()})
-
-        if existing_email:
-            flash("Email address already registered!")
-            return redirect(url_for("register"))
-
-        # add user details to db
-        register = {
-            "firstname": request.form.get("firstname").lower(),
-            "lastname": request.form.get("lastname").lower(),
-            "username": request.form.get("username").lower(),
-            "email": request.form.get("email").lower(),
-            "password": generate_password_hash(request.form.get("password")),
-            "date_created": date.strftime("%d %b %Y")
-        }
-        mongo.db.users.insert_one(register)
-
-        # put the user into a session cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Sign Up Successful!")
-        return redirect(url_for("my_profile", username=session["user"]))
-
-    return render_template("register.html")
-
-
-# Log existing user into site
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        # Check if user already in db
-        existing_user = mongo.db.users.find_one(
-                        {"username": request.form.get("username").lower()})
-        # ensure password matches
-        if existing_user:
-            if check_password_hash(existing_user["password"],
-                                   request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                return redirect(url_for("my_profile",
-                                username=session["user"]))
-            else:
-                # invalid password
-                flash("Incorrect Username/Password!")
-                return redirect(url_for("login"))
-        else:
-            # username doesn't exist/is incorrect
-            flash("Incorrect Usernname/Password")
-            return redirect(url_for("login"))
-
-    return render_template("login.html")
-
-
-# Logs user out of their account
-@app.route("/logout")
-def logout():
-    # remove user from session cookie
-    flash("You have been logged out")
-    session.pop("user")
-    return redirect(url_for("login"))
 
 #blog view
-@app.route("/blog")
-def blog():
-    return render_template("blog.html")
+# Displays all blog posts  in the db
+@app.route("/blogs")
+def blogs():
+    blogs = list(mongo.db.blogs.find())
+    return render_template("blogs.html", blogs=blogs)
+
+
+# Display blog post
+@app.route("/blog_detail/<blog_id>")
+def blog_detail(blog_id):
+    blog = mongo.db.blogs.find_one({"_id": ObjectId(blog_id)})
+    return render_template("blog_detail.html",
+                           blog=blog)
+
+
+# Allow users to add a blog post to db
+@app.route("/add_blog", methods=["GET", "POST"])
+def add_blog():
+    if request.method == "POST":
+        # default values if fields are left blank
+        default_img = ("blogimage.png")
+        blog = {
+            "blog_title": request.form.get("blog_title"),
+            "content": request.form.get("content"),
+            "image": request.form.get("image") or default_img,
+            "created_by": session["user"],
+            "date_created": date.strftime("%d %b %Y"),
+        }
+        mongo.db.blogs.insert_one(blog)
+        flash("Your Blog Post Has Been Added")
+        return redirect(url_for("blogs", username=session["user"]))
+
+    blog = mongo.db.blogs.find().sort("blog_title", 1)
+    return render_template("add_blog.html", blogs=blogs)
+
+
+# Update blog post
+@app.route("/edit_blog/<blog_id>", methods=["GET", "POST"])
+def edit_blog(blog_id):
+    if request.method == "POST":
+        # default values if fields are left blank
+        default_img = ("blog_image.png")
+        update = {
+            "blog_title": request.form.get("blog_title"),
+            "content": request.form.get("content"),
+            "image": request.form.get("image") or default_img,
+            "created_by": session["user"],
+            "date_created": date.strftime("%d %b %Y")
+        }
+        mongo.db.blogs.update({"_id": ObjectId(blog_id)}, update)
+        flash("Your Blog Post has been updated")
+        return redirect(url_for("blogs", username=session["user"]))
+
+    blog = mongo.db.blogs.find_one({"_id": ObjectId(blog_id)})
+    blogs = mongo.db.blogs.find().sort("blog_title", 1)
+    print(blog)
+    return render_template("edit_blog.html", blog=blog, blogs=blogs)
+
+
+# Allow users to delete blog post
+@app.route("/delete_blog/<blog_id>")
+def delete_blog(blog_id):
+    mongo.db.blogs.remove({"_id": ObjectId(blog_id)})
+    flash("Blog Post has been deleted")
+    return redirect(url_for("blogs", username=session["user"]))
 
 
 #events view
@@ -208,12 +198,11 @@ def page_not_found(e):
 # Run the App
 # =================
 
+
 if __name__ == "__main__":
-
-    app.run(host=os.environ.get("FLASK_IP"),
-            port=int(os.environ.get("FLASK_PORT")),
-            debug=os.environ.get("FLASK_DEBUG"))
-
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=False)
     
 
 
